8297246cfc5d9fde3fc6763080bee527c3ac9965
Sean Anih <sean.anih@gmail.com>
2020-10-15 10:27:31 -0400

    added basic styling and connected app to API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import './App.css';
 
-function App() {
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import CharGrid from './components/characters/CharGrid';
+import Header from './components/ui/Header';
+
+const App = () => {
+  const [chars, setChars] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await Axios(`https://www.breakingbadapi.com/api/characters`)
+
+      setChars(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [])
+
   return (
     <div className="container">
-      App
+      <Header />
+      <CharGrid isLoading={isLoading} characters={chars} />
     </div>
   );
 }
---
+++ src/components/characters/CharItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CharItem = ({ char }) => {
+    
+    return (
+        <div>
+            {char.name}
+        </div>
+    )
+}
+
+export default CharItem
